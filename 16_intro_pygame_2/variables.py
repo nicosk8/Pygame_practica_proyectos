eaf6eb9596c66a3108abfa5ca35a9e92d8fc9011
nicosk8 <nicoledeniseheck@gmail.com>
2025-10-23 12:06:16 -0300

    subida inicial

---
+++ 16_intro_pygame_2/variables.py
@@ -0,0 +1,31 @@
+# SCREEN
+ASPECT_RATIO = (800, 600)
+CENTER_SCREEN = (ASPECT_RATIO[0]//2 , ASPECT_RATIO[1]//2)
+# =========================
+
+# COLORES
+COLOR_AZUL = (0, 0, 255)
+
+
+COLORES = {
+    "azul": COLOR_AZUL,
+    "verde": (0, 255, 0),
+    "rojo": (255, 0, 0),
+    "blanco": (255, 255, 255),
+    "negro": (0, 0, 0),
+    "rosa": (255, 192, 203)
+}
+# =========================
+
+# DIMENSIONES IMAGENES
+DIMENSION_ESFERA = [100, 100]
+# =========================
+
+# CONFIGS MOVIMIENTO
+PIXELES_MOV = 25
+# =========================
+
+# RUTAS IMAGENES
+PATH_ESFERA_IMG = '16_intro_pygame/1_star.png'
+FUENTE_ALAGARD = '16_intro_pygame_2/alagard.ttf'
+# =========================
